a85886d64c537c46676d31ac692abc1b88e0510d
Chris Fincher <chris@chrisfincher.com>
2014-01-06 17:50:35 -0800

    Initial commit.

---
+++ unjumbler.py
@@ -0,0 +1,8 @@
+__author__ = 'Chris Fincher'
+
+def unjumble(letters, list):
+    print 'hello world'
+
+if __name__ == '__main__':
+    # TODO Handle parameters
+    unjumble("dog", 'not a list but oh well')
